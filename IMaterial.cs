cbe05e3767ac75bcea5e359f26dae22d4596aa81
FilaCo <dvchizha@edu.hse.ru>
2019-04-19 10:43:37 +0500

    Добавлен интерфейс IMaterial и реализация - твёрдый материал

---
+++ IMaterial.cs
@@ -0,0 +1,12 @@
+﻿namespace PhysEngine
+{
+    /// <summary>
+    /// Интерфейс, представляющий информацию о материале в физическом движке
+    /// </summary>
+    public interface IMaterial
+    {
+        string Name { get; set; }
+        float Density { get; }
+        float Restitution { get; }
+    }
+}
---
+++ RigidMaterial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysEngine
+{
+    struct RigidMaterial : IMaterial
+    {
+        private float _density;
+        private float _restitution;
+
+        public string Name { get; set; }
+
+        public float Density => _density;
+
+        public float Restitution => _restitution;
+
+        public RigidMaterial(string name, float density, float restitution)
+        {
+            Name = name;
+            _density = density;
+            _restitution = restitution;
+        }
+    }
+}
