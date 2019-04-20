cbce14aa9ee0955911e8240c8c784170b84742fa
FilaCo <dvchizha@edu.hse.ru>
2019-04-20 14:03:55 +0500

    Замена большого класса многообразия на составные интерфейсы

---
+++ ICollider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhysEngine
+{
+    /// <summary>
+    /// Представляет структуру простого многообразия, хранящего информацию о коллизии двух объектов <see cref="Body"/>
+    /// </summary>
+    public interface ICollider
+    {
+        Body FirstBody { get; }
+        Body SecondBody { get; }
+        /// <summary>
+        /// Проверяет, столкнулись ли 
+        /// </summary>
+        bool AreCollided();
+        /// <summary>
+        /// Инициализирует 
+        /// </summary>
+        void Initialize();
+        /// <summary>
+        /// Прикладывает импульс к системе из двух тел
+        /// </summary>
+        void ApplyImpulse();
+    }
+}
