5814d48ad75b784a7e1fc0953d784595d55e199c
FilaCo <dvchizha@edu.hse.ru>
2019-04-21 03:12:15 +0500

    Добавлены комментарии к интерфейсу ICollider

--- ICollider.cs
+++ ICollider.cs
@@ -1,9 +1,19 @@
 ﻿namespace PhysEngine
 {
+    /// <summary>
+    /// Интерфейс, описывающий коллайдер для двух объектов, реализующих интерфейс <see cref="IBody"/>
+    /// </summary>
     interface ICollider
     {
+
         IBody First { get; }
+
         IBody Second { get; }
+
+        /// <summary>
+        /// Проверяет, имеется ли коллизия между двумя телами
+        /// </summary>
+        /// <returns>Возвращает true, если между телами произошла коллизия, иначе false</returns>
         bool AreCollided();
     }
 }
