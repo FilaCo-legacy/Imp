57796649af2f5f31ce8fb2ebc6ec23d60fc8a633
FilaCo <dvchizha@edu.hse.ru>
2019-04-21 03:25:52 +0500

    Описан интерфейс для форм тел объектов

--- IShape.cs
+++ IShape.cs
@@ -1,37 +1,14 @@
-﻿using System;
-using System.Drawing;
-
-namespace PhysEngine
+﻿namespace PhysEngine
 {
     /// <summary>
-    /// Перечисление, определяющее возможные формы
+    /// Интерфейс, описывающий форму объекта
     /// </summary>
-    public enum TypeShape { CIRCLE, POLY, COUNT };
-    /// <summary>
-    /// Интерфейс, описывающий абстрактную форму
-    /// </summary>
-    public interface IShape:ICloneable
-    {        
-        Body Body { get; set; }
+    public interface IShape
+    {
         /// <summary>
-        /// Тип фигуры
+        /// Вычисляет площадь формы
         /// </summary>
-        TypeShape GetType { get; }        
-        /// <summary>
-        /// Посчитать массу для связанного объекта
-        /// </summary>
-        /// <param name="origin">Ссылка на связанные объект</param>
-        void ComputeMass();
-        /// <summary>
-        /// Инициализирует новый объект, реализующий интерфейс <see cref="IShape"/>
-        /// </summary>
-        void Initialize();
-        /// <summary>
-        /// Отрисовка фигуры внутри движка
-        /// </summary>
-        /// <param name="g">Объект, на котором происходит отрисовка</param>
-        /// <param name="scale">Масштаб</param>
-        void Draw(Graphics g, int scale);
-        void SetOrient(float angleRadians);
+        /// <returns></returns>
+        float CalculateArea();
     }
 }
