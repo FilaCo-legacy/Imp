d06965057d8b3158cc6c5c1ac8c21a5f9a2ee0a6
FilaCo <filaco@dchv-pc.filaco.org>
2019-05-08 21:38:40 +0500

    Добавление комментариев к структуре AABB

--- CollisionDetection/BroadPhase/AABB.cs
+++ CollisionDetection/BroadPhase/AABB.cs
@@ -3,18 +3,28 @@
 namespace PhysEngine.CollisionDetection.BroadPhase
 {
     /// <summary>
-    /// Структура, представляющая собой прямоугольник, выровненный по осям координат (Axis-Aligned Bounding Box)
+    /// Axis-Aligned Bounding Box
     /// </summary>
+    // ReSharper disable once InconsistentNaming
     public struct AABB
     {
+        /// <summary>
+        /// Coordinates of a left-upper corner of the <see cref="AABB"/>
+        /// </summary>
         public Vector LeftUpper { get; }
 
+        /// <summary>
+        /// Coordinates of a right-lower corner of the <see cref="AABB"/>
+        /// </summary>
         public Vector RightLower { get; }
-
+        
         public float Width => RightLower.X - LeftUpper.X;
 
         public float Height => RightLower.Y - LeftUpper.Y;
 
+        /// <summary>
+        /// Coordinates of a center point of the <see cref="AABB"/>
+        /// </summary>
         public Vector Center => 0.5f * (LeftUpper + RightLower);
 
         public AABB(float x, float y, float width, float height)
@@ -29,6 +39,12 @@ namespace PhysEngine.CollisionDetection.BroadPhase
             RightLower = rightLower;
         }
 
+        /// <summary>
+        /// Checks if the AABBs are collided or not
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public static bool AreCollided(AABB a, AABB b)
         {
             if (a.RightLower.X < b.LeftUpper.X || a.LeftUpper.X > b.RightLower.X)            
